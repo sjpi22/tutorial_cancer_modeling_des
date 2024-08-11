f532d4236130c7db209821f4a1bfcc7841ca0142
sjpi22 <124000054+sjpi22@users.noreply.github.com>
2024-08-10 18:48:22 -0700

    Streamline IMABC code

--- R/03_calibration_general_functions.R
+++ R/03_calibration_general_functions.R
@@ -15,7 +15,7 @@ recursive_read_csv <- function(l_files) {
   return(file_data)
 }
 
-# General parameters for calibration
+# Load general parameters for calibration
 load_calib_params <- function(l_params_all, # Model parameters to update
                               target_files, # List of file paths with targets
                               prior_file = 'data/priors.rds', # Path to parameter mapping as .rds file
@@ -76,6 +76,176 @@ load_calib_params <- function(l_params_all, # Model parameters to update
   
 }
 
+add_IMABC_params <- function(l_params_calib,
+                             alpha_current = 0.0001,
+                             alpha_stop = 0.05,
+                             use_seed = FALSE,
+                             N_start_multiplier = 1000,
+                             optional_args = list()) {
+  
+  # Define Model Parameters/Priors
+  param_df <- with(l_params_calib, {
+    data.frame(
+      name_var = prior_map$var_id,
+      dist_var = prior_map$distr,
+      min = prior_map$min,
+      max = prior_map$max
+    )
+  })
+  
+  priors <- as.priors(
+    param_df,
+    parameter_name = "name_var", dist_base_name = "dist_var"
+  )
+  
+  # Reshape true targets, get mapping, and calculate confidence intervals for true targets given alpha values
+  l_true_reshaped <- reshape_calib_targets(l_params_calib$l_true_targets,
+                                           output_map = TRUE)
+  
+  # Calculate confidence intervals
+  ci <- calc_target_ci(l_params_calib$l_true_targets,
+                       alpha = list(current = alpha_current, stopping = alpha_stop),
+                       reshape_outputs = TRUE)
+  
+  # Define Target Values
+  target_df <- with(l_true_reshaped, {
+    
+    target_df <- data.frame(
+      target_groups = make.names(target_map$target_groups),
+      target_names = names(v_targets),
+      targets = unname(v_targets),
+      current_lower_bounds = ci$current$lb,
+      current_upper_bounds = ci$current$ub,
+      stopping_lower_bounds = ci$stopping$lb,
+      stopping_upper_bounds = ci$stopping$ub
+    )
+    
+    return(target_df)
+  })
+  
+  targets <- as.targets(target_df)
+  
+  # Define Target Function
+  fn <- function(v_params_update) {
+    v_targets <- with(l_params_calib, {
+      params_to_calib_targets(l_params_all, v_params_update, prior_map, 
+                              v_ages)
+    })
+    return(v_targets)
+  }
+  
+  target_fun <- define_target_function(
+    targets, priors, FUN = fn, use_seed = use_seed
+  )
+  
+  # Assign N_start
+  N_start = N_start_multiplier * length(priors)
+  
+  # IMABC core parameters
+  imabc_inputs <- list(
+    target_fun = target_fun,
+    priors = priors,
+    targets = targets,
+    N_start = N_start,
+    seed = l_params_calib$l_params_all$seed)
+  
+  imabc_inputs <- c(imabc_inputs,
+                    optional_args)
+  
+  # Return augmented calibration parameter list
+  l_params_calib <- c(l_params_calib, 
+                      fn = fn,
+                      use_seed = use_seed,
+                      imabc_inputs = list(imabc_inputs))
+  
+  return(l_params_calib)
+}
+
+# Calculate 1-alpha confidence intervals given alpha as a single value, vector, or list
+calc_target_ci <- function(l_calib_targets,
+                           alpha = 0.05,
+                           reshape_outputs = TRUE) {
+  # Check if alpha is a list
+  if (is.list(alpha)) {
+    ci <- list()
+    for (label in names(alpha)) {
+      ci[[label]] <- calc_target_ci_internal(l_calib_targets, alpha[[label]], reshape_outputs = TRUE)
+    }
+  } else {
+    ci <- calc_target_ci_internal(l_calib_targets, alpha[[label]], reshape_outputs = TRUE)
+  }
+  
+  return(ci)
+}
+
+# Calculate confidence intervals of calibration targets given alpha as a single value or vector
+calc_target_ci_internal <- function(l_calib_targets,
+                                    alpha = 0.05, 
+                                    reshape_outputs = TRUE) {
+  
+  # If alpha is a single value, turn into vector
+  if (is.numeric(alpha) & (length(alpha) == 1)) {
+    alpha <- rep(alpha, length(l_calib_targets))
+  } else {
+    # Check that alpha is a vector of length equal to number of targets
+    assert_that(is.numeric(alpha) & (length(alpha) == length(l_calib_targets)),
+                msg = "alpha should be a single value or vector with length equal to number of targets.")
+  }
+  
+  # Name vector by targets
+  names(alpha) <- names(l_calib_targets)
+  
+  # Loop over targets
+  ci <- list()
+  for (target in names(l_calib_targets)) {
+    # Define parameters
+    if (target == 'prevalence') {
+      # Get binomial exact confidence interval with 'binom' package
+      df_confint <- with(l_calib_targets[[target]], {
+        binom.confint(x = prevalence * n_total, n = n_total, conf.level = 1-alpha[target], methods = "exact")
+      })
+      
+      # Extract CI as list
+      ci[[target]] <- list(lb = df_confint$lower,
+                           ub = df_confint$upper)
+    } else if (target == 'stage_distr') {
+      # Get binomial exact confidence interval with 'binom' package
+      df_confint <- with(l_calib_targets[[target]], {
+        binom.confint(x = count, n = sum(count), conf.level = 1-alpha, methods = "exact")
+      })
+      
+      # Extract CI as list
+      ci[[target]] <- list(lb = df_confint$lower,
+                           ub = df_confint$upper)
+    } else if (target == 'incidence') {
+      # Calculate CI with normal approximation
+      ci[[target]] <- with(l_calib_targets[[target]], {
+        # Width of confidence interval
+        ci_width <- qnorm((1-alpha) + alpha/2) * se
+        
+        # Create CI
+        ci <- list()
+        ci$lb <- pmax(0, incidence - ci_width) # Lower bound
+        ci$ub <- incidence + ci_width # Upper bound
+        
+        return(ci)
+      })
+      
+    }
+  }
+  
+  # If reshape_outputs is TRUE, flatten target outputs
+  if (reshape_outputs) {
+    lb <- ub <- c()
+    for (target in ci) {
+      lb <- c(lb, target$lb)
+      ub <- c(ub, target$ub)
+    }
+    return(list(lb = lb,
+                ub = ub))
+  } else return (ci)
+}
+
 # Run model and generate calibration target outputs
 calc_calib_targets <- function(l_params_all, m_patients, 
                                v_ages,
@@ -121,93 +291,54 @@ calc_calib_targets <- function(l_params_all, m_patients,
 }
 
 # Reshape calibration targets to single vector
-reshape_calib_targets <- function(l_calib_targets, output_se = FALSE, 
-                                  output_map = FALSE,
-                                  conf_level = 0.95) {
+reshape_calib_targets <- function(l_calib_targets, 
+                                  output_map = FALSE) {
   # Initialize vector(s) of outputs
   v_targets <- c()
-  if (output_se) {
-    v_se <- c()
-    z <- qnorm(conf_level + (1-conf_level)/2, 0, 1)
-  }
   if (output_map) {
     target_map <- data.frame()
   }
   
-  # Prevalence by lesion type
-  prev_df <- l_calib_targets[['prevalence']] %>%
-    mutate(varname = paste('prev', age_start, age_end, sep = '_'))
-  
-  # Extract prevalence and name as varname
-  v_prev <- prev_df$prevalence
-  names(v_prev) <- prev_df$varname
-  
-  # Append to target vector
-  v_targets = c(v_targets, v_prev)
-  
-  # Get standard error
-  if (output_se) {
-    v_temp_se <- (prev_df$ci_ub - prev_df$ci_lb) / (2 * z)
-    names(v_temp_se) <- prev_df$varname
-    v_se <- c(v_se, v_temp_se)
-  }
-  
-  # Get output mapping
-  if (output_map) {
-    target_map <- rbind(target_map,
-                        data.frame(target_groups = paste("Prevalence of precancerous lesion"),
-                                   target_index = (prev_df$age_start + prev_df$age_end)/2))
+  # Loop over target list
+  for (target in names(l_calib_targets)) {
+    # Age-specific targets
+    if (grepl("^(prevalence|incidence)", target)) {
+      target_df <- l_calib_targets[[target]] %>%
+        mutate(varname = paste(target, age_start, age_end, sep = '_'))
+      
+      # Select vector of targets
+      add_v_target <- target_df[[target]]
+      
+      # Get output mapping
+      if (output_map) {
+        target_map <- rbind(target_map,
+                            data.frame(target_groups = target,
+                                       target_index = (target_df[['age_start']] + target_df[['age_end']])/2))
+      } 
+    } else { # Non age-specific targets
+      # Stage distribution
+      target_df <- l_calib_targets[[target]] %>%
+        mutate(varname = paste(target, stage_dx, sep = '_'))
+      
+      # Select vector of targets
+      add_v_target <- target_df[['pct']]
+      
+      # Get output mapping
+      if (output_map) {
+        target_map <- rbind(target_map,
+                            data.frame(target_groups = target,
+                                       target_index = target_df$stage_dx))
+      }
+    }
     
-  }
-  
-  # Extract incidence and name as varname and append to target vector
-  inc_df <- l_calib_targets[['incidence']] %>%
-    mutate(varname = paste('inc', 'cancer', age_start, age_end, sep = '_'))
-  v_inc <- inc_df$incidence
-  names(v_inc) <- inc_df$varname
-  v_targets = c(v_targets, v_inc)
-  
-  # Get standard error
-  if (output_se) {
-    v_temp_se <- inc_df$se
-    names(v_temp_se) <- inc_df$varname
-    v_se <- c(v_se, v_temp_se)
-  }
-  
-  # Get output mapping
-  if (output_map) {
-    target_map <- rbind(target_map,
-                    data.frame(target_groups = "Cancer incidence",
-                               target_index = (inc_df$age_start + inc_df$age_end)/2))
-  }
-  
-  # Stage distribution
-  stage_df <- l_calib_targets[['stage_distr']] %>%
-    mutate(varname = paste('stage', stage_dx, sep = '_'))
-  v_stage <- stage_df$pct
-  names(v_stage) <- stage_df$varname
-  v_targets = c(v_targets, v_stage)
-  
-  # Get standard error
-  if (output_se) {
-    v_temp_se <- (stage_df$ci_ub - stage_df$ci_lb) / (2 * z)
-    names(v_temp_se) <- stage_df$stage_dx
-    v_se <- c(v_se, v_temp_se)
-  }
-  
-  # Get output mapping
-  if (output_map) {
-    target_map <- rbind(target_map,
-                    data.frame(target_groups = "Stage at diagnosis",
-                               target_index = stage_df$stage_dx))
+    # Append to target vector
+    names(add_v_target) <- target_df$varname
+    v_targets = c(v_targets, add_v_target)
   }
   
   # Return outputs
-  if (output_se | output_map) {
+  if (output_map) {
     v_outputs <- list(v_targets = v_targets)
-    if (output_se) {
-      v_outputs$v_se <- v_se
-    } 
     if (output_map) {
       v_outputs$target_map <- target_map
     }
@@ -215,10 +346,12 @@ reshape_calib_targets <- function(l_calib_targets, output_se = FALSE,
   } else return(v_targets)
 }
 
+
 # Wrapper for running calibration and outputting vector of outputs
 params_to_calib_targets <- function(l_params_all, v_params_update, param_map, 
-                                    v_ages, reshape_output = TRUE,
-                                    output_se = FALSE, output_map = FALSE, 
+                                    v_ages, reshape_output = TRUE, 
+                                    extract_vars = NULL,
+                                    output_map = FALSE, 
                                     conf_level = 0.95,
                                     verbose = FALSE) {
   # Update parameters
@@ -234,8 +367,8 @@ params_to_calib_targets <- function(l_params_all, v_params_update, param_map,
                                         v_ages,
                                         verbose = verbose)
   if (reshape_output) {
-    v_calib_targets <- reshape_calib_targets(l_calib_targets, output_se = output_se,
-                                             output_map = output_map, conf_level = 0.95)
+    v_calib_targets <- reshape_calib_targets(l_calib_targets,
+                                             output_map = output_map)
     return(v_calib_targets)
   } else {
     return(l_calib_targets)
@@ -264,7 +397,7 @@ calc_and_plot_calib_targets <- function(l_true_targets,
     # Get number of targets
     v_lengths[target] <- nrow(l_true_targets[[target]])
   }
-                
+  
   # Summarize target model outputs
   model_UB_95 = apply(m_target_outputs, 2, quantile, probs = c(1),  na.rm = TRUE)
   model_LB_95 = apply(m_target_outputs, 2, quantile, probs = c(0),  na.rm = TRUE)
--- analysis/calibration/IMABC/run_IMABC.R
+++ analysis/calibration/IMABC/run_IMABC.R
@@ -31,14 +31,23 @@ sapply(distr.sources, source, .GlobalEnv)
 
 ###### 2.1 file paths 
 target_files <- list(prevalence = "data/prevalence_asymptomatic_cancer.csv",
-  incidence = "data/incidence_symptomatic_cancer.csv",
-  stage_distr = "data/stage_distr.csv")
+                     incidence = "data/incidence_symptomatic_cancer.csv",
+                     stage_distr = "data/stage_distr.csv")
 
-###### 2.12 model parameters 
+###### 2.2 model parameters 
 n_cohort_calib <- 500000
 seed_calib <- 42
-outpath <- 'output/calibration/IMABC'
 
+###### 2.3 IMABC parameters 
+outpath <- 'output/calibration/IMABC'
+alpha_current <- c(1e-4, 1e-15, 1e-15)
+alpha_stop = c(0.05, 1e-7, 1e-7)
+fn_use_seed <- FALSE
+optional_args = list(
+  N_centers = 1,
+  Center_n = 100,
+  N_post = 2000
+)
 
 #### 3. Pre-processing actions  ===========================================
 
@@ -52,6 +61,13 @@ l_params_calib <- load_calib_params(l_params_init,
                                     seed_calib = seed_calib,
                                     outpath = outpath)
 
+# Add IMABC-specific parameters
+l_params_calib <- add_IMABC_params(l_params_calib,
+                                   alpha_current = alpha_current,
+                                   alpha_stop = alpha_stop,
+                                   use_seed = fn_use_seed,
+                                   optional_args = optional_args)
+
 # Set number of cores to use
 if(is.numeric(Sys.getenv("SLURM_NTASKS_PER_NODE"))) {
   # use the environment variable SLURM_NTASKS_PER_NODE to set
@@ -61,87 +77,15 @@ if(is.numeric(Sys.getenv("SLURM_NTASKS_PER_NODE"))) {
   registerDoParallel(cores=detectCores(logical = TRUE) - l_params_calib$n_cores_reserved_local)  
 }
 
-# Reshape true targets and get mapping
-l_true_reshaped <- reshape_calib_targets(l_params_calib$l_true_targets, output_se = TRUE, output_map = TRUE)
-
-#### 4. IMABC inputs  ===========================================
-
-# Define Model Parameters/Priors
-param_df <- with(l_params_calib, {
-  data.frame(
-    name_var = prior_map$var_id,
-    dist_var = prior_map$distr,
-    min = prior_map$min,
-    max = prior_map$max
-  )
-})
-
-priors <- as.priors(
-  param_df,
-  parameter_name = "name_var", dist_base_name = "dist_var"
-)
-
-# Multiplier for SD to get bounds
-alpha_current = rep(0.0001, length(l_true_reshaped$v_targets))
-alpha_current[l_true_reshaped$target_map$target_groups == 'Cancer incidence'] <- 1e-15
-alpha_current[l_true_reshaped$target_map$target_groups == 'Stage at diagnosis'] <- 1e-15
-
-alpha_stop = rep(0.05, length(l_true_reshaped$v_targets))
-alpha_stop[l_true_reshaped$target_map$target_groups == 'Cancer incidence'] <- 1e-7
-alpha_stop[l_true_reshaped$target_map$target_groups == 'Stage at diagnosis'] <- 1e-7
-
-
-# Define Target Values
-target_df <- data.frame(
-  target_groups = make.names(l_true_reshaped$target_map$target_groups),
-  target_names = names(l_true_reshaped$v_targets),
-  targets = unname(l_true_reshaped$v_targets),
-  current_lower_bounds = pmax(0, 
-                              unname(l_true_reshaped$v_targets) - qnorm((1-alpha_current) + alpha_current/2) * unname(l_true_reshaped$v_se)),
-  current_upper_bounds = unname(l_true_reshaped$v_targets) + qnorm((1-alpha_current) + alpha_current/2) * unname(l_true_reshaped$v_se),
-  stopping_lower_bounds = pmax(0, 
-                               unname(l_true_reshaped$v_targets) - qnorm((1-alpha_stop) + alpha_stop/2) * unname(l_true_reshaped$v_se)),
-  stopping_upper_bounds = unname(l_true_reshaped$v_targets) + qnorm((1-alpha_stop) + alpha_stop/2) * unname(l_true_reshaped$v_se)
-)
-
-targets <- as.targets(target_df)
-
-# Define Target Function
-fn <- function(v_params_update) {
-  v_targets <- with(l_params_calib, {
-    params_to_calib_targets(l_params_all, v_params_update, prior_map, 
-                            v_ages)
-  })
-  return(v_targets)
-}
-
-target_fun <- define_target_function(
-  targets, priors, FUN = fn, use_seed = FALSE
-)
+#### 4. Run IMABC  ===========================================
 
 # Calibrate model - see here for documentation: https://github.com/c-rutter/imabc/tree/a58a3b7c8db18948ff87fb6be55c6175399f41a2
 start_time <- Sys.time()
 calibration_results <- with(l_params_calib, {
-  imabc(
-    priors = priors,
-    targets = targets,
-    target_fun = target_fun,
-    seed = l_params_all$seed,
-    N_start = 1000 * length(priors),
-    N_centers = 1,
-    Center_n = 100,
-    # output_directory = outpath,
-    N_post = 2000,
-    verbose = TRUE,
-    max_iter = 1000
-    # validate_run = TRUE
-  )
+  do.call(imabc, imabc_inputs)
 })
 end_time <- Sys.time()
-print(end_time - start_time) # 29.41799 mins
-
-# If output_directory is included, Getting error: Error in length(dots) == 2 && class(dots[[1]]) != "list" : 
-# 'length = 2' in coercion to 'logical(1)'
+print(end_time - start_time)
 
 print('Saving output')
 saveRDS(calibration_results, file = file.path(outpath, 'IMABC_outputs.rds'))
